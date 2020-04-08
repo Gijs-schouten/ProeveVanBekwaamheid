ca661f6e78ad9c3ab25486e743dfce7891af7cd7
Gijs Schouten <gijswow@gmail.com>
2020-04-08 14:14:23 +0200

    character customization start

---
+++ Assets/Scripts/ActivationManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationManager : MonoBehaviour {
+	[SerializeField] private GameObject[] _menuButtons;
+	[SerializeField] private GameObject _activeGameObject;
+
+	private void Start() {
+		Subscribe(_menuButtons, Clicked);
+	}
+
+	private void Clicked(GameObject obj) {
+		_activeGameObject.SetActive(false);
+		_activeGameObject = obj;
+		_activeGameObject.SetActive(true);
+	}
+
+	public void Subscribe(GameObject[] buttons, Action<GameObject> method) {
+		for (int i = 0; i < buttons.Length; i++) {
+			buttons[i].GetComponent<Button>().Clicked += method;
+		}
+	}
+}
---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+	public GameObject obj;
+	public event Action<GameObject> Clicked;
+
+	public void Click() {
+		Clicked?.Invoke(obj);
+	}
+}
