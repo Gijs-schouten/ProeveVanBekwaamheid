251243abfeb966aa7c812f47ab903301eaeb3b96
BorahanO <boraoxal@gmail.com>
2020-04-20 14:51:02 +0200

    Health is now displayed as hearts

---
+++ Assets/ScriptsBora/Health/Health.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    int health;
+    [SerializeField]
+    int heartsAmount;
+    
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+
+        if(health > heartsAmount)
+        {
+            health = heartsAmount;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            } else {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < heartsAmount)
+            {
+                hearts[i].enabled = true;
+            } else {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
