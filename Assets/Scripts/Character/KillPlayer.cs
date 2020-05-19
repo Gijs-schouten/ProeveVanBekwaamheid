3afe9fb6dd8fea21b2d8944f9473970cee721901
wessel vink <vinkwessel@hotmail.com>
2020-05-19 14:34:00 +0200

    Pause implemented, deathscreen implemente

---
+++ Assets/Scripts/Character/KillPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+    void Update()
+    {
+        if(transform.position.y < -30)
+        {
+            anim.SetBool("isOpen", true);
+        }
+    }
+}
