3f894eda859db1fc3d8caa27ddf83c8085aed4ea
wessel vink <vinkwessel@hotmail.com>
2020-05-20 15:28:24 +0200

    AnimtionController for customization

---
+++ Assets/Scripts/Animator/PlayerAnimations.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField]private Jump _jump;
+    [SerializeField] private PlayerMovement _playerMovement;
+    [SerializeField] Animator _anim;
+
+    void Start()
+    {
+        _jump.JumpAction += JumpAnimationTrigger;
+        _playerMovement.WalkAction += WalkAnimationTrigger;
+    }
+
+
+    private void JumpAnimationTrigger(bool isJumping)
+    {
+        _anim.SetBool("isJumping", isJumping);
+    }
+
+    private void WalkAnimationTrigger(bool isWalking)
+    {
+        _anim.SetBool("Open", isWalking);
+    }
+
+}
+
--- Assets/Scripts/Character/Jump.cs
+++ Assets/Scripts/Character/Jump.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Jump : MonoBehaviour
 {
     public float JumpForce;
+    public event Action <bool> JumpAction ;
+
+
     [SerializeField] private LayerMask _groundLayer;
     private PlayerMovement _movement;
-
     private BoxCollider2D _boxCollider2D;
     private Rigidbody2D _rB;
+
+
 
     void Start()
     {
@@ -22,9 +27,9 @@ public class Jump : MonoBehaviour
     {
         if(IsGrounded() && Input.GetKeyDown(KeyCode.Space))        {
             _rB.velocity = Vector2.up * JumpForce;
+            Jumping(true);
         }
         if (!IsGrounded()){
-            Debug.Log("fart");
             _movement.movementSpeed = 10f;
         }
     }
@@ -32,7 +37,14 @@ public class Jump : MonoBehaviour
     {
        RaycastHit2D raycastHit2D = Physics2D.BoxCast(_boxCollider2D.bounds.center, _boxCollider2D.size, 0f, Vector2.down, .1f, _groundLayer);
         _movement.movementSpeed = 15f;
-       return raycastHit2D.collider != null;
+        Jumping(false);
+        return raycastHit2D.collider != null;
+        
     }
  
+    private void Jumping(bool isJumping)
+    {
+        JumpAction?.Invoke(isJumping);
+    }
+
 }
