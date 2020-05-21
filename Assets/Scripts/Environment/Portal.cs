cc587979560f73a685a0703c90b968ea2df85a8f
Gijs Schouten <gijswow@gmail.com>
2020-05-21 12:38:58 +0200

    practice level completed, portal completed

---
+++ Assets/Scripts/Environment/Portal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+	private SceneLoader _loader;
+	[SerializeField] private GameObject _player;
+
+	[SerializeField] private float _phaseOneDelay;
+	[SerializeField] private float _phaseTwoDelay;
+
+	private void Start() {
+		_loader = GetComponent<SceneLoader>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if(collision.tag == "Player") {
+			print("cool");
+			StartCoroutine("StartTeleport");
+		}
+	}
+
+	private IEnumerator StartTeleport() {
+		GetComponent<ActivateObject>().SwitchActiveObject();
+		_player.GetComponent<TestMovement>().enabled = false;
+		yield return new WaitForSeconds(_phaseOneDelay);
+		StartCoroutine("TeleportPlayer");
+	}
+
+	private IEnumerator TeleportPlayer() {
+		Destroy(_player);
+		GetComponent<ActivateObject>().SwitchActiveObject();
+		yield return new WaitForSeconds(_phaseTwoDelay);
+		_loader.LoadScene();
+	}
+
+}
